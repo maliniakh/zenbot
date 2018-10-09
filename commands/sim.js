7916b40bff28f02d5585fe923116baeac10925ac
maliniak <maliniakh@gmail.com>
2018-10-09 18:10:46 +0200

    poprawka przy generowaniu arffa, lbIdx zamiast step, ale chyba i tak dobrze dzialalo
    ahead nie pojedyczne tylko np ahead{1,5}

--- commands/sim.js
+++ commands/sim.js
@@ -98,65 +98,65 @@ module.exports = function (program, conf) {
       var cursor, reversing, reverse_point
       var query_start = so.start ? tb(so.start).resize(so.period_length).subtract(so.min_periods + 2).toMilliseconds() : null
 
-        function exitSim () {
-          console.log()
-          if (!s.period) {
-            console.error('no trades found! try running `zenbot backfill ' + so.selector.normalized + '` first')
-            process.exit(1)
-          }
-          var option_keys = Object.keys(so)
-          var output_lines = []
-          option_keys.sort(function (a, b) {
-            if (a < b) return -1
-            return 1
-          })
-          var options = {}
-          option_keys.forEach(function (k) {
-            options[k] = so[k]
-          })
+      function exitSim () {
+        console.log()
+        if (!s.period) {
+          console.error('no trades found! try running `zenbot backfill ' + so.selector.normalized + '` first')
+          process.exit(1)
+        }
+        var option_keys = Object.keys(so)
+        var output_lines = []
+        option_keys.sort(function (a, b) {
+          if (a < b) return -1
+          return 1
+        })
+        var options = {}
+        option_keys.forEach(function (k) {
+          options[k] = so[k]
+        })
 
-            let options_output =options
-            options_output.simresults = {}
+        let options_output =options
+        options_output.simresults = {}
 
-          if (s.my_trades.length) {
-            s.my_trades.push({
-              price: s.period.close,
-              size: s.balance.asset,
-              type: 'sell',
-              time: s.period.time
-            })
-          }
-          s.balance.currency = n(s.net_currency).add(n(s.period.close).multiply(s.balance.asset)).format('0.00000000')
-          s.balance.asset = 0
-          s.lookback.unshift(s.period)
-          var profit = s.start_capital ? n(s.balance.currency).subtract(s.start_capital).divide(s.start_capital) : n(0)
-          output_lines.push('end balance: ' + n(s.balance.currency).format('0.00000000').yellow + ' (' + profit.format('0.00%') + ')')
-          //console.log('start_capital', s.start_capital)
-          //console.log('start_price', n(s.start_price).format('0.00000000'))
-          //console.log('close', n(s.period.close).format('0.00000000'))
-          var buy_hold = s.start_price ? n(s.period.close).multiply(n(s.start_capital).divide(s.start_price)) : n(s.balance.currency)
-          //console.log('buy hold', buy_hold.format('0.00000000'))
-          var buy_hold_profit = s.start_capital ? n(buy_hold).subtract(s.start_capital).divide(s.start_capital) : n(0)
-          output_lines.push('buy hold: ' + buy_hold.format('0.00000000').yellow + ' (' + n(buy_hold_profit).format('0.00%') + ')')
-          output_lines.push('vs. buy hold: ' + n(s.balance.currency).subtract(buy_hold).divide(buy_hold).format('0.00%').yellow)
-          output_lines.push(s.my_trades.length + ' trades over ' + s.day_count + ' days (avg ' + n(s.my_trades.length / s.day_count).format('0.00') + ' trades/day)')
-          var last_buy
-          var losses = 0, sells = 0
-          s.my_trades.forEach(function (trade) {
-            if (trade.type === 'buy') {
-              last_buy = trade.price
-            }
-            else {
-              if (last_buy && trade.price < last_buy) {
-                losses++
-              }
-              sells++
-            }
+        if (s.my_trades.length) {
+          s.my_trades.push({
+            price: s.period.close,
+            size: s.balance.asset,
+            type: 'sell',
+            time: s.period.time
           })
-          if (s.my_trades.length) {
-            output_lines.push('win/loss: ' + (sells - losses) + '/' + losses)
-            output_lines.push('error rate: ' + (sells ? n(losses).divide(sells).format('0.00%') : '0.00%').yellow)
+        }
+        s.balance.currency = n(s.net_currency).add(n(s.period.close).multiply(s.balance.asset)).format('0.00000000')
+        s.balance.asset = 0
+        s.lookback.unshift(s.period)
+        var profit = s.start_capital ? n(s.balance.currency).subtract(s.start_capital).divide(s.start_capital) : n(0)
+        output_lines.push('end balance: ' + n(s.balance.currency).format('0.00000000').yellow + ' (' + profit.format('0.00%') + ')')
+        //console.log('start_capital', s.start_capital)
+        //console.log('start_price', n(s.start_price).format('0.00000000'))
+        //console.log('close', n(s.period.close).format('0.00000000'))
+        var buy_hold = s.start_price ? n(s.period.close).multiply(n(s.start_capital).divide(s.start_price)) : n(s.balance.currency)
+        //console.log('buy hold', buy_hold.format('0.00000000'))
+        var buy_hold_profit = s.start_capital ? n(buy_hold).subtract(s.start_capital).divide(s.start_capital) : n(0)
+        output_lines.push('buy hold: ' + buy_hold.format('0.00000000').yellow + ' (' + n(buy_hold_profit).format('0.00%') + ')')
+        output_lines.push('vs. buy hold: ' + n(s.balance.currency).subtract(buy_hold).divide(buy_hold).format('0.00%').yellow)
+        output_lines.push(s.my_trades.length + ' trades over ' + s.day_count + ' days (avg ' + n(s.my_trades.length / s.day_count).format('0.00') + ' trades/day)')
+        var last_buy
+        var losses = 0, sells = 0
+        s.my_trades.forEach(function (trade) {
+          if (trade.type === 'buy') {
+            last_buy = trade.price
           }
+          else {
+            if (last_buy && trade.price < last_buy) {
+              losses++
+            }
+            sells++
+          }
+        })
+        if (s.my_trades.length) {
+          output_lines.push('win/loss: ' + (sells - losses) + '/' + losses)
+          output_lines.push('error rate: ' + (sells ? n(losses).divide(sells).format('0.00%') : '0.00%').yellow)
+        }
         options_output.simresults.start_capital = s.start_capital
         options_output.simresults.last_buy_price = s.last_buy_price
         options_output.simresults.last_assest_value = s.trades[s.trades.length-1].price
@@ -176,62 +176,62 @@ module.exports = function (program, conf) {
         if (so.show_options) {
           output_lines.push(options_json)
         }  output_lines.forEach(function (line) {
-            console.log(line)
-          })
-  if (so.backtester_generation >= 0)
+          console.log(line)
+        })
+        if (so.backtester_generation >= 0)
         {
           fs.writeFileSync(path.resolve(__dirname, '..', 'simulations','sim_'+so.strategy.replace('_','')+'_'+ so.selector.normalized.replace('_','').toLowerCase()+'_'+so.backtester_generation+'.json'),options_json, {encoding: 'utf8'})
         }        if (so.filename !== 'none') {
-            var html_output = output_lines.map(function (line) {
-              return colors.stripColors(line)
-            }).join('\n')
-            var data = s.lookback.slice(0, s.lookback.length - so.min_periods).map(function (period) {
-              var data = {}
-              var keys = Object.keys(period)
-              for(var i = 0;i < keys.length;i++){
-                data[keys[i]] = period[keys[i]]
-              }
-              return data
-            })
-            var code = 'var data = ' + JSON.stringify(data) + ';\n'
-            code += 'var trades = ' + JSON.stringify(s.my_trades) + ';\n'
-            var tpl = fs.readFileSync(path.resolve(__dirname, '..', 'templates', 'sim_result.html.tpl'), {encoding: 'utf8'})
-            var out = tpl
-              .replace('{{code}}', code)
-              .replace('{{trend_ema_period}}', so.trend_ema || 36)
-              .replace('{{output}}', html_output)
-              .replace(/\{\{symbol\}\}/g,  so.selector.normalized + ' - zenbot ' + require('../package.json').version)
-            var out_target = so.filename || 'simulations/sim_result_' + so.selector.normalized +'_' + new Date().toISOString().replace(/T/, '_').replace(/\..+/, '').replace(/-/g, '').replace(/:/g, '').replace(/20/, '') + '_UTC.html'
-            var basename = 'simulations/sim_result_' + so.selector.normalized +'_' + new Date().toISOString().replace(/T/, '_').replace(/\..+/, '').replace(/-/g, '').replace(/:/g, '').replace(/20/, '');
-
-            var data = s.lookback.slice(0, s.lookback.length - so.min_periods).map(function (period) {
-              return {period: {
-                time: period.time,
-                open: period.open,
-                high: period.high,
-                low: period.low,
-                close: period.close,
-                volume: period.volume
-              }}
-            })
-
-            let arff = getInstances(s.lookback, fibonacciLbStrtgy, 2);
-            var ws = new require('stream');
-            ws.writable = true;
-            var output = "";
-            ws.write = function(buf) {
-              // output += buf;
-              fs.appendFileSync(basename + '.arff', buf);
+          var html_output = output_lines.map(function (line) {
+            return colors.stripColors(line)
+          }).join('\n')
+          var data = s.lookback.slice(0, s.lookback.length - so.min_periods).map(function (period) {
+            var data = {}
+            var keys = Object.keys(period)
+            for(var i = 0;i < keys.length;i++){
+              data[keys[i]] = period[keys[i]]
             }
-            arff.writeToStream(ws);
+            return data
+          })
+          var code = 'var data = ' + JSON.stringify(data) + ';\n'
+          code += 'var trades = ' + JSON.stringify(s.my_trades) + ';\n'
+          var tpl = fs.readFileSync(path.resolve(__dirname, '..', 'templates', 'sim_result.html.tpl'), {encoding: 'utf8'})
+          var out = tpl
+            .replace('{{code}}', code)
+            .replace('{{trend_ema_period}}', so.trend_ema || 36)
+            .replace('{{output}}', html_output)
+            .replace(/\{\{symbol\}\}/g,  so.selector.normalized + ' - zenbot ' + require('../package.json').version)
+          var out_target = so.filename || 'simulations/sim_result_' + so.selector.normalized +'_' + new Date().toISOString().replace(/T/, '_').replace(/\..+/, '').replace(/-/g, '').replace(/:/g, '').replace(/20/, '') + '_UTC.html'
+          var basename = 'simulations/sim_result_' + so.selector.normalized +'_' + new Date().toISOString().replace(/T/, '_').replace(/\..+/, '').replace(/-/g, '').replace(/:/g, '').replace(/20/, '')
 
-            // fs.writeFileSync(basename + '.arff', output);
-            //fs.writeFileSync(out_target, out);
-            console.log('wrote', out_target)
+          var data = s.lookback.slice(0, s.lookback.length - so.min_periods).map(function (period) {
+            return {period: {
+              time: period.time,
+              open: period.open,
+              high: period.high,
+              low: period.low,
+              close: period.close,
+              volume: period.volume
+            }}
+          })
+
+          let arff = getInstances(s.lookback, fibonacciLbStrtgy, 5)
+          var ws = new require('stream')
+          ws.writable = true
+          var output = ''
+          ws.write = function(buf) {
+            // output += buf;
+            fs.appendFileSync(basename + '.arff', buf)
           }
-simResults.save(options_output)
+          arff.writeToStream(ws)
+
+          // fs.writeFileSync(basename + '.arff', output);
+          //fs.writeFileSync(out_target, out);
+          console.log('wrote', out_target)
+        }
+        simResults.save(options_output)
           .then(() => {          process.exit(0)
-        })
+          })
           .catch((err) => {
             console.error(err)
             process.exit(0)
@@ -313,11 +313,11 @@ simResults.save(options_output)
    * @param lookAhead
    */
   function getInstances(lookBack, lbStrgy, lookAhead) {
-    var resultArr = [];
-    var arff = new Arff.ArffWriter("chuj", Arff.MODE_OBJECT); //todo: dodac nazwe
+    var resultArr = []
+    var arff = new Arff.ArffWriter('chuj', Arff.MODE_OBJECT) //todo: dodac nazwe
 
     for (var step = 0; step < lbStrgy().maxStep; step++) {
-      var lb = lbStrgy().strtgy(step);
+      var lb = lbStrgy().strtgy(step)
       arff.addNumericAttribute('back' + lb + '.low')
       arff.addNumericAttribute('back' + lb + '.high')
       arff.addNumericAttribute('back' + lb + '.open')
@@ -332,62 +332,73 @@ simResults.save(options_output)
         // arff.addNumericAttribute('back' + lb + '.srsi_K')
       }
     }
-    arff.addNumericAttribute('ahead' + lookAhead)
+    for(var k = 1; k <= lookAhead; k ++) {
+      arff.addNumericAttribute('ahead' + k + '.low')
+      arff.addNumericAttribute('ahead' + k + '.high')
+      arff.addNumericAttribute('ahead' + k + '.close')
+    }
 
 
     for (var i = lookAhead; i < lookBack.length - lbStrgy().maxLookback; i++) {
-      var lbPeriods = {};
-      let normFactor = lookBack[i].close; // do normalizacji
+      var lbPeriods = {}
+      let normFactor = lookBack[i].close // do normalizacji
 
       for (step = 0; step < lbStrgy().maxStep; step++) {
-        let lb = lookBack[i + lbStrgy().strtgy(step)];
-        lbPeriods['back' + step + '.low'] = precise(lb.low / normFactor);
-        lbPeriods['back' + step + '.high'] = precise(lb.high / normFactor);
-        lbPeriods['back' + step + '.open'] = precise(lb.open / normFactor);
-        lbPeriods['back' + step + '.close'] = precise(lb.close / normFactor);
-        lbPeriods['back' + step + '.volume'] = precise(lb.volume / normFactor);
+        let lbIdx = lbStrgy().strtgy(step)
+        let lb = lookBack[i + lbIdx]
+        lbPeriods['back' + lbIdx + '.low'] = precise(lb.low / normFactor)
+        lbPeriods['back' + lbIdx + '.high'] = precise(lb.high / normFactor)
+        lbPeriods['back' + lbIdx + '.open'] = precise(lb.open / normFactor)
+        lbPeriods['back' + lbIdx + '.close'] = precise(lb.close / normFactor)
+        lbPeriods['back' + lbIdx + '.volume'] = precise(lb.volume / normFactor)
         if(step < 5) {
-          lbPeriods['back' + step + '.rsi_avg_gain'] = precise(lb.rsi_avg_gain);
-          lbPeriods['back' + step + '.rsi_avg_loss'] = precise(lb.rsi_avg_loss);
-          lbPeriods['back' + step + '.rsi'] = precise(lb.rsi);
-          lbPeriods['back' + step + '.cci'] = precise(lb.cci);
-          // lbPeriods['back' + step + '.srsi_D'] = precise(lb.srsi_D);
-          // lbPeriods['back' + step + '.srsi_K'] = precise(lb.srsi_K);
+          lbPeriods['back' + lbIdx + '.rsi_avg_gain'] = precise(lb.rsi_avg_gain)
+          lbPeriods['back' + lbIdx + '.rsi_avg_loss'] = precise(lb.rsi_avg_loss)
+          lbPeriods['back' + lbIdx + '.rsi'] = precise(lb.rsi)
+          lbPeriods['back' + lbIdx + '.cci'] = precise(lb.cci)
+          // lbPeriods['back' + lbIdx + '.srsi_D'] = precise(lb.srsi_D);
+          // lbPeriods['back' + lbIdx + '.srsi_K'] = precise(lb.srsi_K);
         }
 
         // todo: infinity tez sprawdzic
         for(var k in lbPeriods) {
           if(lbPeriods[k] === null || isNaN(lbPeriods[k])) {
-            throw new Error(k);
+            // throw new Error(k)
           }
         }
       }
-      lbPeriods['ahead' + lookAhead] = lookBack[i - lookAhead].close / normFactor;
 
-      arff.addData(lbPeriods);
-      resultArr.push({period: lbPeriods});
+      // dodajemy wszystkie ahead(1--lookAhead)
+      for(k = 1; k <= lookAhead; k ++) {
+        lbPeriods['ahead' + k + '.low'] = precise(lookBack[i - k].low / normFactor)
+        lbPeriods['ahead' + k + '.high'] = precise(lookBack[i - k].high / normFactor)
+        lbPeriods['ahead' + k + '.close'] = precise(lookBack[i - k].close / normFactor)
+      }
+
+      arff.addData(lbPeriods)
+      resultArr.push({period: lbPeriods})
     }
 
     // arff.writeToStream(process.stdout);
 
-    return arff;
+    return arff
   }
 
-  let fibonacciArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 13, 15, 17, 19, 21, 24, 27, 30, 34, 40, 55, 77, 89, 120, 144, 160, 190, 233, 270, 300, 350, 400, 500, 600];
+  let fibonacciArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 13, 15, 17, 19, 21, 24, 27, 30, 34, 40, 55, 77, 89, 120, 144, 160, 190, 233, 270, 300, 350, 400, 500, 600]
 
   function fibonacciLbStrtgy() {
     return {
       maxStep: fibonacciArr.length,
       maxLookback: fibonacciArr[fibonacciArr.length - 1],
       strtgy: function (step) {
-        return fibonacciArr[step];
+        return fibonacciArr[step]
       }
     }
   }
 
   function precise(x) {
-    return Number.parseFloat(x).toFixed(3);
+    return Number.parseFloat(x).toFixed(3)
   }
-}
+
 }
 
