c260f6f2f53e8a20cff9ffd74003fd0e0fcd2b25
maliniak <maliniakh@gmail.com>
2018-11-16 14:20:32 +0100

    bin debin labeli (zamiast LabelBinarizer)

--- tf/labels.py
+++ tf/labels.py
@@ -1,9 +1,38 @@
 import numpy as np
 
 def binarize(arr: np.ndarray):
-    return arr.size
+    return [tobin(x) for x in arr]
 
 def debinarize(arr: np.ndarray):
-    pass
+    return [frombin(x) for x in arr]
 
+def tobin(label):
+    if label == 'down':
+        return [1, 0, 0]
+    if label == 'flat':
+        return [0, 1, 0]
+    if label == 'up':
+        return [0, 0, 1]
+    raise Exception
+
+def frombin(bin):
+    if bin == [1, 0, 0]:
+        return 'down'
+    if bin == [0, 1, 0]:
+        return 'flat'
+    if bin == [0, 0, 1]:
+        return 'up'
+    raise Exception
+
+
+test_arr = np.asarray(['down',
+ 'flat',
+ 'up',
+ 'flat'])
+
+binarized = binarize(test_arr)
+print(binarized)
+
+debinarized = debinarize(binarized)
+print(debinarized)
 
